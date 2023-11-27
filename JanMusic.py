e13903a3eaca7fc68add0c8d51048ca614b09121
bald0wang <38057320+Bald0Wang@users.noreply.github.com>
2023-11-27 19:14:44 +0800

    Add files via upload

---
+++ JanMusic.py
@@ -0,0 +1,56 @@
+import json
+from os import makedirs
+from os.path import exists
+import requests
+import logging
+import re
+from urllib.parse import urljoin
+import multiprocessing
+from bs4 import BeautifulSoup
+
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s: %(message)s')
+
+BASE_URL = 'https://ssr1.scrape.center'
+TOTAL_PAGE = 10
+
+RESULTS_DIR = 'results'
+exists(RESULTS_DIR) or makedirs(RESULTS_DIR)
+
+def scrape_page(url):
+    """
+    scrape page by url and return its html
+    :param url: page url
+    :return: html of page
+    """
+    logging.info('scraping %s...', url)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        logging.error('get invalid status code %s while scraping %s',
+                      response.status_code, url)
+    except requests.RequestException:
+        logging.error('error occurred while scraping %s', url, exc_info=True)
+
+def main(page):
+    """
+    main process
+    :return:
+    """
+    index_html = scrape_index(page)
+    detail_urls = parse_index(index_html)
+    for detail_url in detail_urls:
+        detail_html = scrape_detail(detail_url)
+        data = parse_detail(detail_html)
+        logging.info('get detail data %s', data)
+        logging.info('saving data to json file')
+        save_data(data)
+        logging.info('data saved successfully')
+
+if __name__ == '__main__':
+    pool = multiprocessing.Pool()
+    pages = range(1, TOTAL_PAGE + 1)
+    pool.map(main, pages)
+    pool.close()
---
+++ main.py
@@ -0,0 +1,27 @@
+import json,os,requests
+from os import makedirs
+from os.path import exists
+RESULTS_DIR = 'results'
+exists(RESULTS_DIR) or makedirs(RESULTS_DIR)
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.56',
+'cookie':'buvid3=56AB3A23-E097-2CBA-857D-9FB7C041677B30516infoc; b_nut=1668522330; i-wanna-go-back=-1; _uuid=D3999E4B-1D910-92AE-43610-43AE1CDC6B5932744infoc; buvid4=382A326A-FC4A-A391-11BA-E10D1DAD8EDA31817-022111522-HJHOGEcBB83neGNRDAkcxA%3D%3D; CURRENT_FNVAL=4048; buvid_fp_plain=undefined; DedeUserID=71623954; DedeUserID__ckMd5=3982e0834640e29f; b_ut=5; rpdid=0zbfVFJdGt|LsmAghLe|42m|3w1OVGh4; nostalgia_conf=-1; LIVE_BUVID=AUTO2416686977395880; hit-new-style-dyn=0; hit-dyn-v2=1; CURRENT_QUALITY=0; blackside_state=1; fingerprint=b47dccb0fe8b71403889170988886713; buvid_fp=b47dccb0fe8b71403889170988886713; SESSDATA=d15bc7a5%2C1685261481%2C35f22%2Ab2; bili_jct=7e31108c50b3732509768f2c6eae788a; sid=7in6oi43; b_lsid=5F7E2FE5_184C36C67A2; innersign=1; PVID=1; bp_video_offset_71623954=733961575500611600; bsource=search_bing'}
+url = 'https://api.bilibili.com/x/web-interface/view/detail?platform=web&bvid=BV1Dh41167W4&aid=206719735&need_operation_card=1&web_rm_repeat=1&need_elec=1&out_referer=&page_no=1&p=1'
+response = requests.get(url,headers=headers).text
+basurl,baseurl_tail = 'https://www.bilibili.com/video/BV1Dh41167W4?p=','&vd_source=1a432a45372ea0a0d1ec88a20d9cef2c'
+res = json.loads(response)
+for id,i in enumerate(res['data']['View']['pages']):
+    # print(i['part'],id+1)
+    id_ = str(id+1)
+    url = basurl+id_+baseurl_tail
+    cmd_call = 'you-get --format=dash-flv360  -o  "'+os.getcwd()+'\\'+RESULTS_DIR+'"\t'+url # mp4 360p
+    os.system(cmd_call)
+files_  = []
+path = os.getcwd()+'\\'+RESULTS_DIR
+for root, dirs, files in os.walk(path):
+    for i in files:
+        if('.mp4' in i):
+            files_.append(i)
+            name = path+'\\'+i.split('.mp4')[0]
+            cmd_call = 'ffmpeg -i "'+name+'.mp4" -f mp3 -vn "'+name+'.mp3"'
+            os.system(cmd_call)
